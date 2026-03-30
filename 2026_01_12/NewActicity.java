b4d25b67ba1cf6ec6fecac71cb9f33eb7df8149d
kacperkirszewski <144985740+kirszewskikacper@users.noreply.github.com>
2026-03-30 13:32:54 +0200

    Add files via upload

---
+++ 2026_01_12/NewActicity.java
@@ -0,0 +1,38 @@
+package com.example.zadanie;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class NewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_new_activity);
+
+        ViewCompat.setOnApplyWindowInsetsListener(
+                findViewById(R.id.main),
+                (v, insets) -> {
+                    Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                    return insets;
+                }
+        );
+
+        String mail = getIntent().getStringExtra("mail");
+        TextView txt = findViewById(R.id.obszar);
+        txt.setText("Zalogowano jako " + mail);
+    }
+
+    public void buton_wyloguj(View view) {
+        finish();
+    }
+}
