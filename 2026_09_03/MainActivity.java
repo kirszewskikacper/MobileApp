7abea6bfc458d13ea14b16093848c8d81ee68774
kacperkirszewski <144985740+kirszewskikacper@users.noreply.github.com>
2026-09-03 21:52:16 +0200

    Dokończono Zadanie

--- 2026_09_03/MainActivity.java
+++ 2026_09_03/MainActivity.java
@@ -1,10 +1,11 @@
-package com.example.myapplication;
+package com.example.egzamin1;
 
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,11 +18,16 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     Button rzut;
+    Button reset;
     ImageView kosc1;
     ImageView kosc2;
     ImageView kosc3;
     ImageView kosc4;
     ImageView kosc5;
+    TextView wynik_gry;
+    TextView wynik_losowania;
+
+    int wynik_ostateczny = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,21 +41,132 @@ public class MainActivity extends AppCompatActivity {
         });
 
         rzut = findViewById(R.id.Rzut);
+        reset = findViewById(R.id.reset);
         kosc1 = findViewById(R.id.kosc1);
         kosc2 = findViewById(R.id.kosc2);
         kosc3 = findViewById(R.id.kosc3);
         kosc4 = findViewById(R.id.kosc4);
         kosc5 = findViewById(R.id.kosc5);
+        wynik_gry = findViewById(R.id.wynik_gry);
+        wynik_losowania = findViewById(R.id.wynik_losowania);
 
         rzut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                int tablicaKosci[] = {(int)Math.random()*5+1,(int)Math.random()*5+1,(int)Math.random()*5+1,(int)Math.random()*5+1,(int)Math.random()*5+1};
+                int tablicaKosci[] = {(int)(Math.random()*6+1),(int)(Math.random()*6+1),(int)(Math.random()*5+1),(int)(Math.random()*5+1),(int)(Math.random()*5+1)};
 
                 int iloscWynikow[] = {0,0,0,0,0,0};
 
                 int wynik = 0;
+
+
+                //ustawianie obrazków
+                switch (tablicaKosci[0]){
+                    case 1:
+                        kosc1.setImageResource(R.drawable.k1);
+                        break;
+                    case 2:
+                        kosc1.setImageResource(R.drawable.k2);
+                        break;
+                    case 3:
+                        kosc1.setImageResource(R.drawable.k3);
+                        break;
+                    case 4:
+                        kosc1.setImageResource(R.drawable.k4);
+                        break;
+                    case 5:
+                        kosc1.setImageResource(R.drawable.k5);
+                        break;
+                    case 6:
+                        kosc1.setImageResource(R.drawable.k6);
+                        break;
+                }
+
+                switch (tablicaKosci[1]){
+                    case 1:
+                        kosc2.setImageResource(R.drawable.k1);
+                        break;
+                    case 2:
+                        kosc2.setImageResource(R.drawable.k2);
+                        break;
+                    case 3:
+                        kosc2.setImageResource(R.drawable.k3);
+                        break;
+                    case 4:
+                        kosc2.setImageResource(R.drawable.k4);
+                        break;
+                    case 5:
+                        kosc2.setImageResource(R.drawable.k5);
+                        break;
+                    case 6:
+                        kosc2.setImageResource(R.drawable.k6);
+                        break;
+                }
+
+                switch (tablicaKosci[2]){
+                    case 1:
+                        kosc3.setImageResource(R.drawable.k1);
+                        break;
+                    case 2:
+                        kosc3.setImageResource(R.drawable.k2);
+                        break;
+                    case 3:
+                        kosc3.setImageResource(R.drawable.k3);
+                        break;
+                    case 4:
+                        kosc3.setImageResource(R.drawable.k4);
+                        break;
+                    case 5:
+                        kosc3.setImageResource(R.drawable.k5);
+                        break;
+                    case 6:
+                        kosc3.setImageResource(R.drawable.k6);
+                        break;
+                }
+
+                switch (tablicaKosci[3]){
+                    case 1:
+                        kosc4.setImageResource(R.drawable.k1);
+                        break;
+                    case 2:
+                        kosc4.setImageResource(R.drawable.k2);
+                        break;
+                    case 3:
+                        kosc4.setImageResource(R.drawable.k3);
+                        break;
+                    case 4:
+                        kosc4.setImageResource(R.drawable.k4);
+                        break;
+                    case 5:
+                        kosc4.setImageResource(R.drawable.k5);
+                        break;
+                    case 6:
+                        kosc4.setImageResource(R.drawable.k6);
+                        break;
+                }
+
+                switch (tablicaKosci[4]){
+                    case 1:
+                        kosc5.setImageResource(R.drawable.k1);
+                        break;
+                    case 2:
+                        kosc5.setImageResource(R.drawable.k2);
+                        break;
+                    case 3:
+                        kosc5.setImageResource(R.drawable.k3);
+                        break;
+                    case 4:
+                        kosc5.setImageResource(R.drawable.k4);
+                        break;
+                    case 5:
+                        kosc5.setImageResource(R.drawable.k5);
+                        break;
+                    case 6:
+                        kosc5.setImageResource(R.drawable.k6);
+                        break;
+                }
+
 
                 for(int i = 0; i< tablicaKosci.length; i++){
                     switch (tablicaKosci[i]){
@@ -78,6 +195,26 @@ public class MainActivity extends AppCompatActivity {
                         wynik = wynik + iloscWynikow[i] * (i+1);
                     }
                 }
+                wynik_ostateczny += wynik;
+
+                wynik_losowania.setText("Wynik tego losowania: " + wynik);
+
+                wynik_gry.setText("Wynik gry: " + wynik_ostateczny);
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                kosc1.setImageResource(R.drawable.question);
+                kosc2.setImageResource(R.drawable.question);
+                kosc3.setImageResource(R.drawable.question);
+                kosc4.setImageResource(R.drawable.question);
+                kosc5.setImageResource(R.drawable.question);
+
+                wynik_ostateczny = 0;
+                wynik_losowania.setText("Wynik tego losowania: 0");
+                wynik_gry.setText("Wynik gry: 0");
             }
         });
 
